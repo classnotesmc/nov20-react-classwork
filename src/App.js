b4e994666e925f3f737b5cb5f7908cfb48bbecfd
RVHU321014UR <RVHU321014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-11-20 20:26:47 -0500

    firt react page project nov 20th

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 function App() {
+  const filePath = "src/App.js";
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p> Hello, my name is {user.firstName} {user.lastName}</p>
+        <p>Also, {formatName(user)}</p>
+        <p>With an ES6 string: {`${user.firstName} ${user.lastName}`}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>{filePath}</code> and save to reload.
         </p>
         <a
           className="App-link"
